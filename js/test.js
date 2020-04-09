1c1a00c4868b726b378a3c69b6f6af206482837a
Evan-The-Orange <63368646+Evan-The-Orange@users.noreply.github.com>
2020-04-08 23:09:28 -0700

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "https://cors-anywhere.herokuapp.com/https://secure.runescape.com/m=hiscore/index_lite.ws?player=iSky",
         headers: {
-            "Origin": "runescape.com"
+            //"Origin": "runescape.com"
         }
     }).done(function(res) {
         console.log(res);
