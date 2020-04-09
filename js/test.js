d18099900599ccf97e94dec813f19411007ccf98
Evan-The-Orange <63368646+Evan-The-Orange@users.noreply.github.com>
2020-04-08 22:49:04 -0700

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://secure.runescape.com/m=hiscore/index_lite.ws?player=The+Orange"
+        url: "https://secure.runescape.com/m=hiscore/index_lite.ws?player=The+Orange",
+        crossDomain: true
     }).done(function(data) {
         alert(data);
     })
