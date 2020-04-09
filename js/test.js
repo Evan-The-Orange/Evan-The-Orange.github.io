ab753f2a4f3ea958960d8404a56fbeef79288547
Evan-The-Orange <63368646+Evan-The-Orange@users.noreply.github.com>
2020-04-08 22:57:53 -0700

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "https://cors-escape.herokuapp.com/https://secure.runescape.com/m=hiscore/index_lite.ws?player=The+Orange",
-        crossDomain: true,
-        dataType:"jsonp"
-    }).done(function(data) {
-        alert(data);
-    })
+    let loadPosts = function () {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (this.readyState === 4 && this.status === 200) {
+                let response = JSON.parse(this.responseText);
+                console.log(response);  
+            } 
+        }
+        xhr.open("GET", " https://cors-escape.herokuapp.com/https://secure.runescape.com/m=hiscore/index_lite.ws?player=The+Orange");
+        xhr.setRequestHeader("Accept", 'application/json');
+        xhr.send();
+      }
 });
